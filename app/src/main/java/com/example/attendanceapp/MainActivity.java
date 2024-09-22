ed63cae2000beb88b71050db3f94adcbd84a4d29
Sharmin <info.sharmin360@gmail.com>
2024-09-22 22:43:59 +0600

    Attendance App

---
+++ app/src/main/java/com/example/attendanceapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.attendanceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<ClassItem> classItems = new ArrayList<>();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab_main);
+        fab.setOnClickListener(view -> showDialog());
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        classAdapter = new ClassAdapter(this, classItems);
+        recyclerView.setAdapter(classAdapter);
+        classAdapter.setOnItemClickListener(position -> gotoItemActivity(position));
+    }
+
+    private void gotoItemActivity(int position) {
+        Intent intent = new Intent(this,StudentActivity.class);
+
+        intent.putExtra("className",classItems.get(position).getClassName());
+        intent.putExtra("subjectName", classItems.get(position).getSubjectName());
+        intent.putExtra("position", position);
+        startActivity(intent);
+    }
+
+
+    private void showDialog() {
+        MyDialog dialog = new MyDialog();
+        dialog.show(getSupportFragmentManager(), MyDialog.CLASS_ADD_DIALOG);
+        dialog.setListener((className, subjectName)->addClass(className,subjectName));
+
+
+    }
+
+    private void addClass(String className,String subjectName) {
+        classItems.add(new ClassItem(className,subjectName));
+        classAdapter.notifyDataSetChanged();
+    }
+
+}
